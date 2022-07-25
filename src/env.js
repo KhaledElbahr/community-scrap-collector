5c58df6a8a31df52effdbb943ddfbe6a27c6f47f
KhaledElbahr <39977044+KhaledElbahr@users.noreply.github.com>
2022-07-25 16:05:31 +0200

    init: csc project

---
+++ src/env.js
@@ -0,0 +1,17 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAF_WzzlKKTTF_lbPSyVCypc0lfhtXvAdI",
+//   authDomain: "csc-iti-project.firebaseapp.com",
+//   projectId: "csc-iti-project",
+//   storageBucket: "csc-iti-project.appspot.com",
+//   messagingSenderId: "489248221903",
+//   appId: "1:489248221903:web:65732f7a8d12bccd4ca5ba"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
