d798ce8eec380d2b321ad9a0b9a99db49f9a78df
Sara <sarakilany13@gmail.com>
2022-07-30 12:54:49 +0200

    feat:about and privacy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Home from './views/home/Home';
-
+import Privacy from './views/privacy/Privacy';
+import About from './views/about/About';
+import ContactUs from './views/contactus/ContactUs'
 function App() {
   return (
     <>
-    <Home />
+
+      <Home />
+      <ContactUs />
+
     </>
   );
 }
